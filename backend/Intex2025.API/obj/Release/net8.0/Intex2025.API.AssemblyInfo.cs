c6d77dfe140914e5e0de2a4f893fe114d8f90287
jaydennguyen815 <jaydennguyen0815@gmail.com>
2025-04-07 10:26:08 -0600

    initial commit

--- backend/Intex2025.API/obj/Release/net8.0/Intex2025.API.AssemblyInfo.cs
+++ backend/Intex2025.API/obj/Release/net8.0/Intex2025.API.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Intex2025.API")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6109b4aa2ac61e079ca4fccf58c9252ea8988c66")]
 [assembly: System.Reflection.AssemblyProductAttribute("Intex2025.API")]
