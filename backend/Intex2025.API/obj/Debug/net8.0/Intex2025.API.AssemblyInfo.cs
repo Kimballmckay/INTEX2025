8d54ce1b49bdd8d180c69bfb9f5f524af4776168
sarahbboyer <sarahbboyer22@gmail.com>
2025-04-09 09:57:59 -0600

    backend packages

--- backend/Intex2025.API/obj/Debug/net8.0/Intex2025.API.AssemblyInfo.cs
+++ backend/Intex2025.API/obj/Debug/net8.0/Intex2025.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Intex2025.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c015e8f78969b16140154588050e103ef41bc266")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+645f418b1e92d41b19da11f98970ee75daa282fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Intex2025.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Intex2025.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
